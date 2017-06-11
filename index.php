5e6a8831def386f3cc6931946633b3aab33d8732
fernando <feranimaciones@gmail.com>
2017-06-11 13:11:23 -0600

    Act 2

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-  <!--  <link rel="stylesheet" type="text/css" href="../../css/bootstrap.css"> --->
+  <!--  <link rel="stylesheet" type="text/css" href="../../css/bootstrap.css"> -->
 
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" href="calculadora.css">
